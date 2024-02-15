48224572069ef55e6e84621b7fc5e387be667bb1
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2024-02-14 21:20:53 -0600

    Changed App.js content

--- App.js
+++ App.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("h1", { id: "heading", key: "0" }, "This is React!"),
-  React.createElement("p", { key: "1" }, "This is a paragraph3"),
-  React.createElement("p", { key: "2" }, "This is a paragraph4"),
+  React.createElement("p", { key: "1" }, "This is a paragraph1"),
+  React.createElement("p", { key: "2" }, "This is a paragraph2"),
+  React.createElement("p", { key: "3" }, "This is a paragraph3"),
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
