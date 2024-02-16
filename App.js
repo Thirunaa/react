81cd16e4e0283af38274b8e898952b48e5e6fcd6
Thirunaa <thirunaavukkarasu.thiru@gmail.com>
2024-02-15 21:47:16 -0600

    Included JSX added components

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Navbar from "./src/components/Navbar";
+import Footer from "./src/components/Footer";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("h1", { id: "heading", key: "0" }, "This is React!"),
-  React.createElement("p", { key: "1" }, "This is a paragraph1"),
-  React.createElement("p", { key: "2" }, "This is a paragraph2"),
-  React.createElement("p", { key: "3" }, "This is a paragraph3"),
-]);
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<App />);
